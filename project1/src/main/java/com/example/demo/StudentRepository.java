8f08df26435e278624369d96fec5aca2457243a0
Nam Le <lehainam2371999@gmail.com>
2020-05-19 09:34:32 +0700

    #3 - follow conventions

--- project1/src/main/java/com/example/demo/StudentRepository.java
+++ project1/src/main/java/com/example/demo/StudentRepository.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.demo.Student;
 
-public interface StudentRepository extends CrudRepository<Student, Integer>, StudentRepositoryCustom {
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+
+	@Query("SELECT s FROM Student s WHERE s.firstName LIKE '?1%' OR s.lastName LIKE '?1%'")
+	Iterable<Student> findByFirstNameOrLastName(String search);
 
 }
--- project1/src/main/java/com/example/demo/StudentRepositoryCustom.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo;
-
-public interface StudentRepositoryCustom {
-	public Iterable<Student> findByFirstOrLastName(String name);
-}
